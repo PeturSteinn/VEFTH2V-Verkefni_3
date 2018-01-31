a61242b1b73a17a92465ffe6ff9ab14b21509478
PeturSteinn <peturgu220@nemi.tskoli.is>
2018-01-31 21:53:00 +0000

    Forrit tilbúið

--- forrit.py
+++ forrit.py
@@ -2,33 +2,33 @@ from bottle import route, run, error, static_file, request, template
 
 frettir = [
     {
-        'title':'Róbert fannst í Roblox heima hjá sér eftir fjölmargar fjarvistir í skóla',
+        'title':'Logi fannst í Roblox heima hjá sér eftir fjölmargar fjarvistir í skóla',
         'text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'picture':'',
+        'picture':'/static/robloxlove.png',
         'author':'Óli Geir Waage'
     },
     {
         'title':'Óstöðvandi weeaboo gengur um götur Selfossar',
         'text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'picture':'',
+        'picture':'/static/petur.png',
         'author':'Finnbogi Faggason',
     },
     {
         'title':'Hættulegustu glæpamenn á Íslandi',
         'text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'picture':'value',
+        'picture':'/static/criminals.png',
         'author':'Álfheimar Sólheimar Guðnason',
     },
     {
-        'title':'value',
+        'title':'Háttatíminn á að vera klukkan 8',
         'text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'picture':'value',
+        'picture':'/static/robert_sad.png',
         'author':'Kjörís Emmesís Kjartanson',
     },
     {
-        'title':'value',
+        'title':'Ausur drepa fleiri en 15 manns á ári',
         'text':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'picture':'value',
+        'picture':'/static/einar_rose.png',
         'author':'Bjartur Birtir Facebookson',
     }]
 
@@ -47,6 +47,10 @@ def kt(kennitala):
 @route('/lidurb')
 def lidurB():
     return template('lidurb')
+
+@route('/lidurb/frettir/<n>')
+def lidurB_frett(n):
+    return template('frettir', frettir[int(n)])
 
 @route('/static/<filename>')
 def server_static(filename):
--- verk3.tpl
+++ verk3.tpl
@@ -2,9 +2,10 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="/static/master.css">
     <title>Verkefni 3</title>
   </head>
-  <body>
+  <body class="center">
     <h1>Verkefni 3</h1>
     <a href="/lidura">Liður A</a>
     <a href="/lidurb">Liður B</a>
